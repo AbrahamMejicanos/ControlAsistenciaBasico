aa3d91b6c751bae9825dfa228bec56fd62b57206
Abraham Mejicanos <amejicanos38@gmail.com>
2022-11-27 19:42:54 -0600

    Query concluido

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -34,14 +34,14 @@ CREATE TABLE Empleado(
 	CodigoEmpleado VARCHAR(100) NOT NULL,
 	PrimerNombre VARCHAR(300) NOT NULL,
 	SegundoNombre VARCHAR(300) NOT NULL,
-	FechaNacimiento DATE NOT NULL,
+	FechaNacimiento NVARCHAR(30) NOT NULL,
 	Genero VARCHAR(100) NOT NULL,
 	Email VARCHAR(300) NOT NULL,
 	PaisId INT NOT NULL,
 	Ciudad VARCHAR(100) NOT NULL,
 	Direccion VARCHAR(300) NOT NULL,
 	Telefono VARCHAR(100) NOT NULL,
-	FechaContratacion DATE NOT NULL,
+	FechaContratacion NVARCHAR(30) NOT NULL,
 	Estado BIT NOT NULL,
 	FechaCreacion NVARCHAR(30) NOT NULL,
 	FechaModificacion NVARCHAR(30) NULL,
@@ -53,10 +53,10 @@ GO
 CREATE TABLE MarcacionesEmpleados(
 	Id INT IDENTITY(1,1) NOT NULL,
 	EmpleadoId INT NOT NULL,
-	HoraEntrada DATETIME NOT NULL,
-	HoraSalida DATETIME NOT NULL,
-	FechaCreacion DATETIME NOT NULL,
-	FechaModificacion DATETIME NOT NULL,
+	HoraEntrada NVARCHAR(30) NOT NULL,
+	HoraSalida NVARCHAR(30) NOT NULL,
+	FechaCreacion NVARCHAR(30) NOT NULL,
+	FechaModificacion NVARCHAR(30) NULL,
 	PRIMARY KEY(Id),
 	FOREIGN KEY(EmpleadoId) REFERENCES Empleado(Id)
 );
@@ -91,6 +91,23 @@ BEGIN
 		FechaCreacion AS "F. CREACION",
 		FechaModificacion AS "F. MODIFICACION"
 	FROM Sucursal
+END
+GO
+
+CREATE PROCEDURE __EmpleadoIndex
+AS
+BEGIN
+	SELECT 
+		Id AS "ID",
+		SucursalId AS "ID SUCURSAL",
+		CodigoEmpleado AS "C. EMPLEADO",
+		CONCAT(PrimerNombre,' ',SegundoNombre) AS "NOMBRE",
+		Genero AS "GENERO",
+		Email AS "CORREO",
+		CONCAT(PaisId, '|', Ciudad, '|', Direccion) AS "RESIDENCIA",
+		Telefono AS "TELEFONO",
+		Estado AS "ESTADO"
+	FROM Empleado
 END
 GO
 -- Fin Procedimientos Consultas Generales
@@ -222,6 +239,141 @@ GO
 
 
 
+-- Inicio Procedimientos Empleado
+CREATE PROCEDURE __EmpleadoInsert
+	@SUCURSALID INT,
+	@CODIGOEMPLEADO VARCHAR(100),
+	@PRIMERNOMBRE VARCHAR(300),
+	@SEGUNDONOMBRE VARCHAR(300),
+	@FECHANACIMIENTO NVARCHAR(30),
+	@GENERO VARCHAR(100),
+	@EMAIL VARCHAR(300),
+	@PAISID INT,
+	@CIUDAD VARCHAR(100),
+	@DIRECCION VARCHAR(300),
+	@TELEFONO VARCHAR(100)
+AS
+BEGIN
+	INSERT INTO Empleado (
+		SucursalId,
+		CodigoEmpleado,
+		PrimerNombre,
+		SegundoNombre,
+		FechaNacimiento,
+		Genero,
+		Email,
+		PaisId,
+		Ciudad,
+		Direccion,
+		Telefono,
+		FechaContratacion,
+		Estado,
+		FechaCreacion,
+		FechaModificacion
+	) VALUES (
+		@SUCURSALID,
+		@CODIGOEMPLEADO,
+		@PRIMERNOMBRE,
+		@SEGUNDONOMBRE,
+		@FECHANACIMIENTO,
+		@GENERO,
+		@EMAIL,
+		@PAISID,
+		@CIUDAD,
+		@DIRECCION,
+		@TELEFONO,
+		SYSDATETIME(),
+		1,
+		SYSDATETIME(),
+		NULL
+	)
+END
+GO
+
+CREATE PROCEDURE __EmpleadoUpdate
+	@SUCURSALID INT,
+	@CODIGOEMPLEADO VARCHAR(100),
+	@PRIMERNOMBRE VARCHAR(300),
+	@SEGUNDONOMBRE VARCHAR(300),
+	@FECHANACIMIENTO NVARCHAR(30),
+	@GENERO VARCHAR(100),
+	@EMAIL VARCHAR(300),
+	@PAISID INT,
+	@CIUDAD VARCHAR(100),
+	@DIRECCION VARCHAR(300),
+	@TELEFONO VARCHAR(100),
+	@ID INT
+AS
+BEGIN
+	UPDATE Empleado
+	SET
+		SucursalId = @SUCURSALID,
+		CodigoEmpleado = @CODIGOEMPLEADO,
+		PrimerNombre = @PRIMERNOMBRE,
+		SegundoNombre = @SEGUNDONOMBRE,
+		FechaNacimiento = @FECHANACIMIENTO,
+		Genero = @GENERO,
+		Email = @EMAIL,
+		PaisId = @PAISID,
+		Ciudad = @CIUDAD,
+		Direccion = @DIRECCION,
+		Telefono = @TELEFONO,
+		FechaModificacion = SYSDATETIME()
+	WHERE
+		Id = @ID
+END
+GO
+
+CREATE PROCEDURE __EmpleadoDelete
+	@ID INT
+AS
+BEGIN
+	DELETE FROM Empleado
+	WHERE Id = @ID
+END
+GO
+-- Fin Procedimientos Empleado
+
+-- MARCAR
+
+CREATE PROCEDURE __Marcar
+	@CODIGO NVARCHAR(30),
+	@HORAENTRADA NVARCHAR(30),
+	@HORASALIDA NVARCHAR(30)
+AS
+BEGIN
+	INSERT INTO MarcacionesEmpleados (
+		EmpleadoId,
+		HoraEntrada,
+		HoraSalida,
+		FechaCreacion,
+		FechaModificacion
+	) VALUES (
+		(SELECT Id FROM Empleado WHERE CodigoEmpleado = @CODIGO),
+		@HORAENTRADA,
+		@HORASALIDA,
+		SYSDATETIME(),
+		NULL
+	)
+END
+GO
+
+
+CREATE PROCEDURE __VerMarcaciones
+AS
+BEGIN
+	SELECT
+		Empleado.CodigoEmpleado AS 'CODIGO EMPLEADO',
+		CONCAT(Empleado.PrimerNombre,' ',Empleado.SegundoNombre) AS 'NOMBRE',
+		MarcacionesEmpleados.HoraEntrada AS 'HORA ENTRADA',
+		MarcacionesEmpleados.HoraSalida AS 'HORA SALIDA'
+	FROM MarcacionesEmpleados
+	INNER JOIN Empleado
+	ON MarcacionesEmpleados.EmpleadoId = Empleado.Id
+END
+GO
+-- FIN MARCAR
+
 -- PRUEBAS
 exec __TurnosIndex
 GO
@@ -230,5 +382,11 @@ GO
 exec __SucursalesIndex
 GO
 exec __SucursalInsert 'Primera', 14
+GO
+exec __EmpleadoIndex
+GO
+exec __EmpleadoInsert 2, 'AMEJICANOS', 'ABRAHAM', 'MEJICANOS', '07/05/2003', 'Masculino', 'amejianos38@gmail.com', 91, 'Guatemala', 'Vista al Valle Oriente', '5332 8043'
+GO
+exec __Marcar 'AMEJICANOS', '08:00', '17:00'
 GO
 -- FIN PRUEBAS
